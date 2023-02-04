604b67963549b8a75be0c42ce89e8066ebec7383
Deni <dv04111993@yandex.ru>
2023-02-04 13:27:52 +0100

    added designed Products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,46 @@
 import React from "react"
+import "./Products.scss"
 
 function Products() {
-  return <div>Products</div>
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" />
+            <label>Hats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="first" value="first" name="price" />
+            <label htmlFor="first">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="second" value="second" name="price" />
+            <label htmlFor="second">Price (Highest first)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Products
